4ef5902407a04dbc093990c8b9b8d7d7af2d6827
Sean-mn <b01080080329@gmail.com>
2025-05-20 23:43:38 +0900

    포인터 연습 예제 생성

---
+++ Pointer/Pointer_practice.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+double ToCelsius(double f);
+
+int main() {
+    double temperature = 32;
+    ToCelsius(temperature);
+
+    cout << temperature << endl;
+
+    return 0;
+}
+
+double ToCelsius(double f){
+    return (f - 32) / 1.8f;
+}
